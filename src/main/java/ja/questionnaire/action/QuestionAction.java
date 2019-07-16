afcd276cb44500432009408436e2d98646a816a5
FukushimaTakeshi <whippet.818@gmail.com>
2019-07-16 21:58:53 +0900

    [WIP] validator

--- src/main/java/ja/questionnaire/action/QuestionAction.java
+++ src/main/java/ja/questionnaire/action/QuestionAction.java
@@ -1,19 +1,19 @@
 package ja.questionnaire.action;
 
-import antlr.StringUtils;
-import ja.questionnaire.domain.model.question.Answer;
 import ja.questionnaire.domain.model.question.Question;
 import ja.questionnaire.domain.model.question.QuestionsAndAnswers;
 import ja.questionnaire.form.QuestionForm;
 import ja.questionnaire.service.AnswerService;
 import ja.questionnaire.service.QuestionService;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
-import org.seasar.framework.util.StringUtil;
 import org.seasar.struts.annotation.ActionForm;
 import org.seasar.struts.annotation.Execute;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -60,9 +60,20 @@ public class QuestionAction {
     public ActionMessages validateAnswers() {
         ActionMessages errors = new ActionMessages();
 
+        List<String> radioQuestions = new ArrayList<>();
+        List<Question> questions = questionService.findAll(form.questionnaireId).list();
+        for (Question question : questions) {
+            if (question.getType().equals("0002")) {
+                radioQuestions.add(question.getId());
+            }
+        }
+
         for (Map.Entry<String, String> answerMap : form.answers.entrySet()) {
-            if (StringUtil.isEmpty(answerMap.getValue())) {
+            if (StringUtils.isEmpty(answerMap.getValue())) {
                 errors.add(GLOBAL_MESSAGE, new ActionMessage("未回答の質問があります", false));
+            } else if (radioQuestions.contains(answerMap.getKey()) &&
+                    (!NumberUtils.isNumber(answerMap.getKey()) || !NumberUtils.isNumber(answerMap.getValue()))) {
+                    errors.add(GLOBAL_MESSAGE, new ActionMessage("不正な値があります", false));
             }
         }
         return errors;
